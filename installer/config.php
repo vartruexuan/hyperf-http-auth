f8f03a438cc7e8a3456f34386d4cfff5242987bf
李铭昕 <715557344@qq.com>
2019-07-01 11:38:46 +0800

    Update config.php

--- installer/config.php
+++ installer/config.php
@@ -47,7 +47,7 @@ return [
                 1 => [
                     'name' => 'yes',
                     'packages' => [
-                        'hyperf/model-cache',
+                        'hyperf/di',
                     ],
                 ],
             ],
