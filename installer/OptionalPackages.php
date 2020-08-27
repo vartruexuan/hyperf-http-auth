05abe17aba414a020a4b9bc2644e976a6ed3fa1c
李铭昕 <715557344@qq.com>
2020-08-27 15:19:40 +0800

    Format code.

--- installer/OptionalPackages.php
+++ installer/OptionalPackages.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
@@ -512,7 +512,7 @@ class OptionalPackages
         $this->io->write('<info>Adding .gitattributes</info>');
         $ignoreFile = sprintf('%s/.gitattributes', $this->projectRoot);
         file_put_contents($ignoreFile, '/tests export-ignore' . PHP_EOL, FILE_APPEND);
-        file_put_contents($ignoreFile, '/.github export-ignore', FILE_APPEND);
+        file_put_contents($ignoreFile, '/.github export-ignore' . PHP_EOL, FILE_APPEND);
     }
 
     /**
