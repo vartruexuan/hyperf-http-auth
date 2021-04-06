647698bd05b06778a42343714a404a88790279b1
guozhaoxuan <1175398345@qq.com>
2021-04-06 18:07:59 +0800

    初始化代码

---
+++ src/Annotation/FreeLogin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by
+ * User: GuoZhaoXuan
+ * Date: 2020/12/22
+ * Time: 9:23
+ */
+
+namespace HyperfHttpAuth\Annotation;
+use Hyperf\Di\Annotation\AbstractAnnotation;
+
+/**
+ * 免登录注解
+ * @Annotation
+ * @Target({"METHOD","CLASS"})
+ */
+class FreeLogin extends AbstractAnnotation
+{
+
+}
